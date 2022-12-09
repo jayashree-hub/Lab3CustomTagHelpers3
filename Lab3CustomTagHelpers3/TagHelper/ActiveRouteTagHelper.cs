76a17db87325d03299724be612ad774afc1f402f
lenovo <lenovo@DESKTOP-T854KS0>
2022-12-09 22:56:39 +0000

    Add project files.

---
+++ Lab3CustomTagHelpers3/TagHelper/ActiveRouteTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Lab3CustomTagHelpers3.TagHelper
+{
+    [HtmlTargetElement(Attributes = "asp-active-route")]
+    public class ActiveRouteTagHelper : TagHelper
+    {
+        [HtmlAttributeName("asp-controller")]
+        public string Controller { get; set; }
+
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+
+        [HtmlAttributeName("asp-hide-if-active")]
+        public bool HideIfActive { get; set; }
+
+
+    }
+   
+   
+    
+}
